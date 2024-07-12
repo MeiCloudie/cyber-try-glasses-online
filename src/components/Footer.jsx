763b5393ba42e9e6739cd7d84ded9bf6c17ab517
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-07-12 17:36:42 +0700

    feat: Update Header and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 // import React from "react"
 
 const Footer = () => {
-  return <div className=""></div>
+  return (
+    <div className="bg-pink-700 text-white font-bold text-center p-4">
+      <p>
+        Copyright © {new Date().getFullYear()}{" "}
+        <a
+          href="https://startbootstrap.com/previews/heroic-features"
+          target="_blank"
+          className="hover:text-purple-400"
+        >
+          Start BootStrap
+        </a>{" "}
+        - Truong Thuc Van (
+        <a
+          href="https://github.com/MeiCloudie"
+          target="_blank"
+          className="hover:text-purple-400"
+        >
+          MeiCloudie
+        </a>
+        )
+      </p>
+    </div>
+  )
 }
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 // import React from "react"
 
 const Header = () => {
-  return <div className=""></div>
+  return (
+    <div className="text-center bg-pink-700 p-8">
+      <h1 className="text-white font-bold text-4xl">TRY GLASSES APP ONLINE</h1>
+    </div>
+  )
 }
 
 export default Header
