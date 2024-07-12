90a7c8a55472060d790a27abaf0431f63bbb5398
Truong Thuc Van <truongthucvan1242@gmail.com>
2024-07-12 17:28:46 +0700

    feat: Init Layout componnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
   return (
-    <>
-      <h1 className="bg-red-600">Hello World</h1>
-    </>
+    <div className="flex flex-col h-screen justify-between">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Body.jsx
@@ -0,0 +1,7 @@
+// import React from "react"
+
+const Body = () => {
+  return <div className="mb-auto container"></div>
+}
+
+export default Body
